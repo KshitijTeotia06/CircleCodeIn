ce16e07250655c3a8a48e7fee509025b7b6d2aef
Kshitij T <teotia.kshitij@gmail.com>
2020-01-19 10:51:05 -0800

    Added print messages

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ display_surface = pygame.display.set_mode((X, Y))
 pygame.display.set_caption('Image')
 c1R = int(input("Enter radius for circle 1:"))
 c2R = int(input("Enter radius for circle 2:"))
-dis = int(input("Enter distance between the two circles"))
+dis = int(input("Enter distance between the two circles:"))
 cocentric = False
 tangent = False
 two = False
@@ -15,15 +15,19 @@ zero = False
 if dis == c1R + c2R:
     tangent = True
     image = pygame.image.load("tangent.jpg")
+    print("The two circles are tangent.")
 elif dis == 0:
     cocentric = True
     image = pygame.image.load('cocentric.png')
+    print("The two circles are cocentric.")
 elif dis < c1R+c2R:
     two = True
     image = pygame.image.load('two.jpg')
+    print("The two circles have two points of intersection.")
 elif dis > c1R + c2R:
     zero = True
     image = pygame.image.load('zero.jpg')
+    print("The two circles have zero points of intersection.")
 
 while True:
 
